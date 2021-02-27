2f54349b4ab7ac54cfba0ae3612f5739c598566d
Dario Jejer <dariojejer@gmail.com>
2021-02-27 12:12:24 -0300

    Develop "Create Activity" Form

---
+++ DoSmart/Controllers/ActivitiesController.cs
@@ -0,0 +1,47 @@
+﻿using DoSmart.Models;
+using DoSmart.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Web.Mvc;
+
+namespace DoSmart.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        public ActivitiesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ApplicationDbContext _context { get; set; }
+
+
+        public ActionResult Create()
+        {
+            var viewModel = new ActivityFormViewModel();
+            viewModel.PageHeader = "Add an Activity";
+            return View("ActivityForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Create(ActivityFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.PageHeader = "Add an Activity";
+                return View("ActivityForm", viewModel);
+            }
+            var activity = new Activity()
+            {
+                Title = viewModel.Title,
+                Content = viewModel.Content,
+                CreatorId = User.Identity.GetUserId(),
+                Date = DateTime.Now
+            };
+
+            _context.Activities.Add(activity);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DoSmart/ViewModels/ActivityFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DoSmart.ViewModels
+{
+    public class ActivityFormViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PageHeader { get; set; }
+
+    }
+}
