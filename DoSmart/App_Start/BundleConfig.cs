58be96ae6bbb15bdbcc3d3a114c5a64dac231f18
Dario Jejer <dariojejer@gmail.com>
2021-02-28 00:03:57 -0300

    Initial disposition for Activities

--- DoSmart/App_Start/BundleConfig.cs
+++ DoSmart/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace DoSmart
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/grid.css"));
         }
     }
 }
